a72932a40b11cc81f32a30d467b1296467b892b1
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-04-15 10:35:43 -0300

    projeto mercado api java

---
+++ src/test/java/br/com/fiap/mercado_api/MercadoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mercado_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
